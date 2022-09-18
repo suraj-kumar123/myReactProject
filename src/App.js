24ff67aa3ad9fb38eecc3063b9faecb155a14978
Suraj kumar <surajmesinane@gmail.com>
2022-09-18 01:11:57 -0400

    changes to attain dynamic data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 //added new branch
-function App() {
+function App(props) {
   return (
-    <div> Hello World </div>
+    <header>
+      <h1> Welcome to {props.name} Kitchen </h1>
+    </header>
   );
+}
+// const menu = [
+//   "Borritos",
+//   "Tacos",
+//   "Quesadilla",
+//   "Empanadas"
+// ]
+export function Footer(props){
+  return(
+    <footer>
+      <p> Copyright {props.year}</p>
+    </footer>
+  )
+
+}
+
+export function Main(props) {
+  return (
+    <section>
+      <h2> Menu </h2>
+      <ul>
+        {props.menu.map((dish) => (
+          <li key = {dish.id}>{dish.title}</li>
+        ))}
+      </ul>
+    </section>
+  )
 }
 
 export default App;
